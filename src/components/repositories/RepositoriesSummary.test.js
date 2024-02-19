7d572438a3ae3cf6d4c45f33bfdad58cbfe9662a
RelancioBorgesDev <relancio.dev@gmail.com>
2024-02-19 15:12:38 -0300

    feat: adicionado teste para conferir o display de informaçoes sobre o repositório

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -14,3 +14,19 @@ test("displays the primary language of the repository", () => {
 
   expect(language).toBeInTheDocument();
 });
+
+test("displays information about the repository", () => {
+  const repository = {
+    language: "Javascript",
+    stargazers_count: 10,
+    forks: 30,
+    open_issues: 1,
+  };
+  render(<RepositoriesSummary repository={repository} />);
+
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(new RegExp(`\\b${value}\\b`));
+    expect(element).toBeInTheDocument();
+  }
+});
